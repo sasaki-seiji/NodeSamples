6dd9de697b1fae6ad33ebe4dce47d6fcf6e8c2ac
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-01-30 22:14:53 +0900

    socket.io with express sample

---
+++ socketio/overview/app-with-express.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3000);
+
+app.get('/', function (req, res) {
+	// 2018.01.30 change: deprecated
+  //res.sendfile(__dirname + '/index-with-express.html');
+  res.sendFile(__dirname + '/index-with-express.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
