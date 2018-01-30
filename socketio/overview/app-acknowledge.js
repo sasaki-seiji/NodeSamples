356687d8da36379d82b82497c31e1476ff1f9236
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-01-30 23:14:24 +0900

    socket.io with acknowledge

---
+++ socketio/overview/app-acknowledge.js
@@ -0,0 +1,27 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(3000);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index-acknowledge.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.on('connection', function (socket) {
+  socket.on('ferret', function (name, fn) {
+  	console.log(name);
+    fn('woot');
+  });
+});
+
+
