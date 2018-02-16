92417fb115e24d3b18c312cc39c1084669357e4a
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-02-17 07:13:41 +0900

    CRUD: insert documents

---
+++ mongodb/crud/insert.js
@@ -0,0 +1,37 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  const db = client.db(dbName);
+
+  // Insert a single document
+  db.collection('inserts').insertOne({a:1}, function(err, r) {
+    assert.equal(null, err);
+    assert.equal(1, r.insertedCount);
+
+		// debug
+		console.log('insertOne inserted: ', r.ops);
+		
+    // Insert multiple documents
+    db.collection('inserts').insertMany([{a:2}, {a:3}], function(err, r) {
+      assert.equal(null, err);
+      assert.equal(2, r.insertedCount);
+
+		// debug
+		console.log('insertMany inserted: ', r.ops);
+		
+      client.close();
+    });
+  });
+});
+
