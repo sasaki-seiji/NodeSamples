aafd057b707311169e336c75bddb4c555772ccf0
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-02-18 07:34:32 +0900

    CRUD: bulk write operations API

---
+++ mongodb/crud/bulk-write-operations.js
@@ -0,0 +1,39 @@
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  const db = client.db(dbName);
+
+  const col = db.collection('bulkops');
+  // Create ordered bulk, for unordered initializeUnorderedBulkOp()
+  var bulk = col.initializeOrderedBulkOp();
+  // Insert 10 documents
+  for(var i = 0; i < 10; i++) {
+    bulk.insert({a: i});
+  }
+
+  // Next perform some upserts
+  for(var i = 0; i < 10; i++) {
+    bulk.find({b:i}).upsert().updateOne({b:1});
+  }
+
+  // Finally perform a remove operation
+  bulk.find({b:1}).deleteOne();
+
+  // Execute the bulk with a journal write concern
+  bulk.execute(function(err, result) {
+    assert.equal(null, err);
+    client.close();
+  });
+});
