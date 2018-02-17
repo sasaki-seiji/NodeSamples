045a4b23cae428df7e501eb9ddd6044bf167064b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-02-17 18:45:49 +0900

    CRUD: findOneAndDelete()

--- mongodb/crud/findOneAndDelete.js
+++ mongodb/crud/findOneAndDelete.js
@@ -20,34 +20,25 @@ MongoClient.connect(url, function(err, client) {
   col.insert([{a:1}, {a:2}, {a:2}], function(err, r) {
     assert.equal(null, err);
     assert.equal(3, r.result.n);
-    
-    // debug
-    console.log('initial insert: ', r.ops);
 
-    // Modify and return the modified document
-    col.findOneAndUpdate({a:1}, {$set: {b: 1}}, {
-        returnOriginal: false
-// 2018.02.17 modify        
-//      , sort: [[a,1]]
-      , sort: {a:1}
-      , upsert: true
-    }, function(err, r) {
-      assert.equal(null, err);
-      assert.equal(1, r.value.b);
-
-		  // debug
-		  console.log('findOneAndUpdate: ', r.value);
-
-      // Remove and return a document
-      col.findOneAndDelete({a:2}, function(err, r) {
+		// debug
+		console.log('initial insert: ', r.ops);
+		
+    // Remove a document from MongoDB and return it
+    col.findOneAndDelete({a:1}, {
+// 2018.02.17 modify
+        //sort: [[a,1]]
+        sort: {a:1}
+      }
+      , function(err, r) {
         assert.equal(null, err);
         assert.ok(r.value.b == null);
 
 				// debug
 				console.log('findOneAndDelete: ', r.value);
-
+		
         client.close();
-      });
     });
   });
 });
+
