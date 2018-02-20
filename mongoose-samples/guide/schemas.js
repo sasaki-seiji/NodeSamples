e678bcc46369671a36ed793541b8de8e3b835f94
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-02-20 20:12:26 +0900

    guide: schemas

---
+++ mongoose-samples/guide/schemas.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var blogSchema = new Schema({
+  title:  String,
+  author: String,
+  body:   String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs:  Number
+  }
+});
+console.log('blocSchema:\n', blogSchema);  
+
+var Blog = mongoose.model('Blog', blogSchema);
+console.log('Blog:\n', Blog);
