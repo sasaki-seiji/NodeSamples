6e3c97ded75ff7b947a36b74c0e37410317c8ab8
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-02-23 19:46:42 +0900

    connections: options

---
+++ mongoose-samples/connections/options.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var uri = 'mongodb://localhost/myapp';
+
+const options = {
+
+  //WARNING: The `useMongoClient` option is no longer necessary in mongoose 5.x, please remove it.
+  //useMongoClient: true,
+
+  autoIndex: false, // Don't build indexes
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
+};
+mongoose.connect(uri, options);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('connected!');
+  db.close();
+});
+
