293db65ef7039a5c3a1eb1cfc4d7c085c32bdf9d
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-02-24 18:17:25 +0900

    sub docs: adding sub-docs

---
+++ mongoose-samples/subdocuments/adding-sub-docs.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+
+var childSchema = new Schema({ name: 'string' });
+
+var parentSchema = new Schema({
+  // Array of subdocuments
+  children: [childSchema],
+  // Single nested subdocuments. Caveat: single nested subdocs only work
+  // in mongoose >= 4.2.0
+  child: childSchema
+});
+var Parent = mongoose.model('Parent', parentSchema);
+
+
+var Parent = mongoose.model('Parent');
+var parent = new Parent;
+
+// create a comment
+parent.children.push({ name: 'Liesl' });
+var subdoc = parent.children[0];
+console.log(subdoc) // { _id: '501d86090d371bab2c0341c5', name: 'Liesl' }
+console.log('subdoc.isNew? ', subdoc.isNew); // true
+
+parent.save(function (err, parent) {
+  if (err) return handleError(err)
+  console.log('parent: ', parent);
+  
+  db.close();
+});
+
+
